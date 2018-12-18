a8350e5906ebe6a631f9750b1c4cc049d0e9b180
Paulo Henrique <paulo.henrique06@hotmail.com>
2018-12-18 20:30:54 -0200

    fail-fast DateConverter and end chapter 4

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -6,11 +6,13 @@ class DateConverter {
 
     static dataParaTexto(data) {
         // ex: 18/12/2018
-        return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 
     static textoParaData(texto) {
         // ex: new Date(2018, (12 - 1), 18)
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) throw new Error('Deve estar no formato YYYY-MM-DD');
+
         return new Date(...texto.split('-').map((item, indice) => item - parseInt(indice) % 2));
     }
 }
