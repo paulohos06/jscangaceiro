72f20cd9b0215ac48fac827ff65808c6548d10a9
Paulo Henrique <paulo.henrique06@hotmail.com>
2018-12-18 12:31:24 -0200

    end chapter 1

--- client/app/index.js
+++ client/app/index.js
@@ -8,21 +8,21 @@ var tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit', event => {
     event.preventDefault();
-    
+
     var tr = document.createElement('tr');
 
-    // tds data, quantidade e valor
     campos.forEach(campo => {
         var td = document.createElement('td');
         td.textContent = campo.value;
         tr.appendChild(td);
     });
 
-    // td volume
     var tdVolume = document.createElement('td');
     tdVolume.textContent = campos[1].value * campos[2].value;
     tr.appendChild(tdVolume);
 
     tbody.appendChild(tr);
+
+    campos.forEach(campo => campo.value = '');
 });
 
