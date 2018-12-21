74f7444f71515795126e7da2109280ee50825a79
Paulo Henrique <paulo.henrique06@hotmail.com>
2018-12-21 14:22:28 -0200

    Mensagem class and view inheritance

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,11 +9,16 @@ class NegociacaoController {
         this._negociacoes = new Negociacoes();
         this._negociacoesView = new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView('#mensagem');
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso!';
+        this._mensagemView.update(this._mensagem);
         this._negociacoesView.update(this._negociacoes);
         this._limpaFormulario();
     }
