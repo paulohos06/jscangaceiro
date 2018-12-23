b235b330b7a2440a3b06bb19c451a7b9c93dfa2c
Paulo Henrique <paulo.henrique06@hotmail.com>
2018-12-23 20:06:36 -0200

    add a ProxyFactory design pattern

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,23 +6,18 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        // this._negociacoes = new Negociacoes(model => this._negociacoesView.update(model));
-        const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            get(target, prop, receiver) {
-                if(typeof(target[prop]) === typeof(Function) && ['adiciona', 'esvazia'].includes(prop)) {
-                    return function() {
-                        console.log(`"${prop}" disparou a armadilha`);
-                        target[prop].apply(target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return target[prop];
-            }
-        });
+        this._negociacoes = ProxyFactory.create(
+            new Negociacoes(),
+            ['adiciona', 'esvazia'],
+            model => this._negociacoesView.update(model)
+        );
         this._negociacoesView = new NegociacoesView('#negociacoes');
         this._negociacoesView.update(this._negociacoes);
-        this._mensagem = new Mensagem();
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),
+            ['texto'],
+            model => this._mensagemView.update(model)
+        );
         this._mensagemView = new MensagemView('#mensagem');
         this._mensagemView.update(this._mensagem);
     }
@@ -31,14 +26,12 @@ class NegociacaoController {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso!';
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
     apaga() {
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociação apagada com sucesso!';
-        this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario() {
