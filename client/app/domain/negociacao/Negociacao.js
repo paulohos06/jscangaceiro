2de98e869dcd507493b1eb718d3ce6849db11287
Paulo Henrique <paulo.henrique06@hotmail.com>
2018-12-29 13:13:48 -0200

    first equals method in Negotiation class

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -24,4 +24,12 @@ class Negociacao {
     get volume() {
         return this._quantidade * this._valor;
     }
+
+    equals(negociacao) {
+        return this.data.getDate() == negociacao.data.getDate()
+        && this.data.getMonth() == negociacao.data.getMonth()
+        && this.data.getFullYear() == negociacao.data.getFullYear()
+        && this.quantidade == negociacao.quantidade
+        && this.valor == negociacao.valor;
+    }
 }
